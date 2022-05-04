e474803d10c631bdabc6933d569dc4910a9f02ec
bitcamp <bitcamp@DESKTOP-B2G1C5C>
2022-05-04 18:30:14 +0900

    create: 2022.05.04

---
+++ 08.Model2MVCShop(RestFul Server)/src/main/java/com/model2/mvc/web/user/UserRestController.java
@@ -0,0 +1,138 @@
+package com.model2.mvc.web.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.domain.User;
+import com.model2.mvc.service.user.UserService;
+
+
+//==> 회원관리 RestController
+@RestController
+@RequestMapping("/user/*")
+public class UserRestController {
+	
+	///Field
+	@Autowired
+	@Qualifier("userServiceImpl")
+	private UserService userService;
+	//setter Method 구현 않음
+		
+	public UserRestController(){
+		System.out.println(this.getClass());
+	}
+	
+	@Value("#{commonProperties['pageUnit']}")
+	//@Value("#{commonProperties['pageUnit'] ?: 3}")
+	int pageUnit;
+	
+	@Value("#{commonProperties['pageSize']}")
+	//@Value("#{commonProperties['pageSize'] ?: 2}")
+	int pageSize;
+	
+	@RequestMapping( value="json/getUser/{userId}", method=RequestMethod.GET )
+	public User getUser( @PathVariable String userId ) throws Exception{
+		
+		System.out.println("/user/json/getUser : GET");
+		
+		//Business Logic
+		return userService.getUser(userId);
+	}
+
+	@RequestMapping( value="json/login", method=RequestMethod.POST )
+	public User login(	@RequestBody User user,
+									HttpSession session ) throws Exception{
+	
+		System.out.println("/user/json/login : POST");
+		//Business Logic
+		System.out.println("::"+user);
+		User dbUser=userService.getUser(user.getUserId());
+		
+		if( user.getPassword().equals(dbUser.getPassword())){
+			session.setAttribute("user", dbUser);
+		}
+		
+		return dbUser;
+	}
+	
+	@RequestMapping( value="json/addUser", method=RequestMethod.POST )
+	public User addUser(@RequestBody User user)throws Exception{
+
+	System.out.println("/user/json/addUser : POST");
+	//Business Logic
+	
+	userService.addUser(user);
+	
+	return user;
+
+	}
+	
+	@RequestMapping( value="json/updateUser", method=RequestMethod.POST )
+	public User updateUser(@RequestBody User user, HttpSession session)throws Exception{
+
+	System.out.println("/user/json/updateUser : POST");
+	
+	userService.updateUser(user);
+	
+	
+	return user;
+
+	}
+	
+	@RequestMapping( value="json/checkDuplication", method=RequestMethod.POST )
+	public Map checkDuplication(@RequestBody User user)throws Exception{
+
+	System.out.println("/user/json/checkDuplication : POST");
+	
+	boolean result=userService.checkDuplication(user.getUserId());
+	
+	Map map = new HashMap();
+	map.put("result", result);
+	map.put("userId", user.getUserId());
+	
+	
+	return map;
+
+	}
+	
+	@RequestMapping( value="json/getUserList", method=RequestMethod.POST )
+	public Map getUserList(@RequestBody Search search, HttpServletRequest request) throws Exception{
+		
+		System.out.println("/user/json/getUserList : GET/POST");
+				
+		if(search.getCurrentPage() ==0 ){
+			search.setCurrentPage(1);
+		}
+		search.setPageSize(pageSize);
+		
+
+		Map<String , Object> map=userService.getUserList(search);
+		
+		Page resultPage = new Page( search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+		System.out.println(resultPage);
+		
+		map.put("list", map.get("list"));
+		map.put("resultPage", resultPage);
+		map.put("search", search);
+		
+		return map;
+	}
+}
